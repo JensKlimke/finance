bb5d83b7d5a98447ac81b311a48ae8e09206dc59
Jens Klimke <jens.klimke@rwth-aachen.de>
2023-03-10 22:55:04 +0100

    env variables

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,17 +2,26 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { createClient } from 'redis';
 import {v4} from "uuid";
+import cors from 'cors';
 
 dotenv.config();
 
 const app : Express = express();
 const PORT = process.env.PORT || 8000;
 
-const url = `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || '6379'}`;
+const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
+const REDIS_PORT = process.env.REDIS_PORT || '6379';
+
+const url = `redis://${REDIS_HOST}:${REDIS_PORT}`;
 console.log(`Connecting to redis server ${url}`);
 
 const client = createClient({url});
 client.connect().catch(console.error);
+
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
 
 app.get('/', async (req: Request, res: Response) => {
 // get id
