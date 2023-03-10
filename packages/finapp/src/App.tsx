1ff5a7e8786868bc0634739c5a75c10c44822075
Jens Klimke <jens.klimke@rwth-aachen.de>
2023-03-10 22:54:36 +0100

    Log added

--- packages/finapp/src/App.tsx
+++ packages/finapp/src/App.tsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+
 function App() {
   const [key, setKey] = useState<string>();
   useEffect(() => {
-    console.log('RENDER');
-    fetch('http://localhost:8000')
+    console.log(`Connecting to API ${API_URL}`);
+    fetch(API_URL)
       .then(r => r.text())
       .then(r => setKey(r))
       .catch(() => setKey(undefined));
